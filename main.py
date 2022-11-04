2d7d6e4fab088d204df2cf713454f245dff61b90
REZHED <sairus.hedayat@gmail.com>
2022-11-05 00:38:38 +0300

    add

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
-
+from mypcg import select
 import menu
-import select
+
 # os.system('mode con: cols=60 lines=10')
 
 os.system('color')
@@ -13,6 +13,6 @@ text = ""
 # функия для выбора из меню. работает с статическими методами в модули menu
 
 a=menu.Menus.show_menu()
-b=select.select_action(a)
+select.select_action(a)
 
 
