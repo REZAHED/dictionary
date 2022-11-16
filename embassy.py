f944d0da69928ee2579f146fdd262fefbae2eaa0
rezahed <sairus.hedayat@gmail.com>
2022-11-16 11:01:08 +0300

    pyinstaller -c -F -i cm_icon.ico console_monopoly.py

--- embassy.py
+++ embassy.py
@@ -34,29 +34,56 @@ for i,j in dic.items():
 lst =[]
 test = 'سفارت جمهوری اسلامی ایران در مینسک، با ابراز تعارفات خود به وزارت امور خارجه جمهوری بلاروس، احتراماً اشعار می دارد:'
 test2= 'پیرو یادداشت سفارت'
+while True:
+    text = input("Введите текст: ---->>>>>> ")
+    if text =='exit':
+        break
+    # sp = text.split(" ")
+    # print(sp)
 
-text = input("Введите текст: ---->>>>>> ")
-# sp = text.split(" ")
-# print(sp)
-
-# ratio = process.extract(text,lst_big)
-# print(ratio)
-
-
-for i in lst_big:
-
-
-        ratio2 = fuzz.token_set_ratio(text,i[1])
-        print(ratio2)
-
-        if ratio2 >99 :
+    # ratio = process.extract(text,lst_big)
+    # print(ratio)
 
 
-            lst.append([i[0]])
-text_last=''
-for i in lst:
-    text_last +=" ".join(i)+" "
-print(text_last)
+    for i in lst_big:
+
+
+            ratio2 = fuzz.token_set_ratio(text,i[1])
+            ratio3 = fuzz.ratio(text, i[1])
+            ratio4 = fuzz.partial_ratio(text, i[1])
+            ratio5 = fuzz.UQRatio(text, i[1])
+#پیرو یادداشت وزارت امور خارجه
+            #با توجه به سفر اخیر وزیر امور خارجه
+
+
+            print(['T'+str(ratio2) , ratio3,ratio4,ratio5,i[1]])
+
+            if ratio2 >99  and ratio5>20 :
+
+                lst.append([i[0]])
+    # if not lst:
+    #     for i in lst_big:
+    #
+    #         ratio2 = fuzz.token_set_ratio(text, i[1])
+    #         ratio3 = fuzz.ratio(text, i[1])
+    #         # print(['T' + str(ratio2), ratio3, i[1]])
+    #
+    #         if ratio2 > 99 :
+    #             lst.append([i[0]])
+    # if not lst:
+    #     for i in lst_big:
+    #
+    #         ratio2 = fuzz.token_set_ratio(text, i[1])
+    #         ratio3 = fuzz.ratio(text, i[1])
+    #         # print(['T' + str(ratio2), ratio3, i[1]])
+    #
+    #         if ratio2 > 70 :
+    #             lst.append([i[0]])
+    text_last=''
+    for i in lst:
+        text_last +=" ".join(i)+" "
+    print(text_last)
+    lst.clear()
 
 
 # pip install fuzzywuzzy[speedup]
